668eb397a1e7ad6bfde2b876f63b4348a9351c42
su_o0 <plitikyura@gmail.com>
2026-05-07 18:43:49 +0300

    fear: SetupRepository

---
+++ src/Payload/SetupRepository.php
@@ -0,0 +1,54 @@
+<?php
+namespace SuO0\StorageApi\Payload;
+
+use SuO0\StorageApi\Connection\Connection;
+use SuO0\StorageApi\Repository\SearchRepository;
+
+
+use SuO0\StorageApi\Repository\LocationRepository;
+use SuO0\StorageApi\Repository\ContainerRepository;
+use SuO0\StorageApi\Repository\ItemRepository;
+use SuO0\StorageApi\Repository\StockRepository;
+use SuO0\StorageApi\Repository\PartRepository;
+use SuO0\StorageApi\Repository\CarRepository;
+use SuO0\StorageApi\Repository\ItemPhotoRepository;
+use SuO0\StorageApi\Repository\StockPhotoRepository;
+use SuO0\StorageApi\Repository\CarPhotoRepository;
+use SuO0\StorageApi\Repository\SalesArhiveRepository;
+use SuO0\StorageApi\Repository\HistoryRepository;
+use SuO0\StorageApi\Repository\OwnerRepository;
+
+class SetupRepository {
+    public LocationRepository $Location;
+    public ContainerRepository $Container;
+    public ItemRepository $Item;
+    public StockRepository $Stock;
+    public PartRepository $Part;
+    public CarRepository $Car;
+    public ItemPhotoRepository $ItemPhoto;
+    public StockPhotoRepository $StockPhoto;
+    public CarPhotoRepository $CarPhoto;
+    public SalesArhiveRepository $SalesArhive;
+    public HistoryRepository $History;
+    public OwnerRepository $Owner;
+
+    public SearchRepository $Search;
+
+    public function __construct(private \PDO $db, array $config) {
+
+        $this->Location = new LocationRepository($this->db, $config['Location']);
+        $this->Container = new ContainerRepository($this->db, $config['Container']);
+        $this->Item = new ItemRepository($this->db, $config['Item']);
+        $this->Stock = new StockRepository($this->db, $config['Stock']);
+        $this->Part = new PartRepository($this->db, $config['Part']);
+        $this->Car = new CarRepository($this->db, $config['Car']);
+        $this->ItemPhoto = new ItemPhotoRepository($this->db, $config['ItemPhoto']);
+        $this->StockPhoto = new StockPhotoRepository($this->db, $config['StockPhoto']);
+        $this->CarPhoto = new CarPhotoRepository($this->db, $config['CarPhoto']);
+        $this->SalesArhive = new SalesArhiveRepository($this->db, $config['SalesArhive']);
+        $this->History = new HistoryRepository($this->db, $config['History']);
+        $this->Owner = new OwnerRepository($this->db, $config['Owner']);
+
+        $this->Search = new SearchRepository($this->db);
+    }
+}
