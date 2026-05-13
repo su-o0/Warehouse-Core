e9ace588e2f1a15d31c9c0709e332a6cd7ec4090
su_o0 <plitikyura@gmail.com>
2026-05-13 21:49:33 +0300

    fear(config): database, tables

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => '127.0.0.1',
+    'dbname' => 'Storage',
+    'user' => 'devuser',
+    'password' => 'devnon',
+];
---
+++ config/tables.php
@@ -0,0 +1,20 @@
+<?php 
+
+return [
+    'Location' => 'Location',
+    'ContainerPlacement' => 'ContainerPlacement',
+    'ItemPlacement' => 'ItemPlacement',
+    'StockPlacement' => 'StockPlacement',
+    'Container' => 'Container',
+    'PhysicalTag' => 'PhysicalTag',
+    'Item' => 'Item',
+    'Stock' => 'Stock',
+    'Part' => 'Part',
+    'Car' => 'Car',
+    'ItemPhoto' => 'ItemPhoto',
+    'StockPhoto' => 'StockPhoto',
+    'CarPhoto' => 'CarPhoto',
+    'SalesArhive' => 'SalesArhive',
+    'History' => 'History',
+    'Owner' => 'Owner',
+];
