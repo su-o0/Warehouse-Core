c62588e775fe5ed5d2cc19984a76c83284374dfa
su_o0 <plitikyura@gmail.com>
2026-05-15 11:16:45 +0300

    chore(service): Setup addStock

---
+++ src/Scenario/Setup/AddStockService.php
@@ -0,0 +1,37 @@
+<?php 
+namespace SuO0\StorageApi\Service\Setup;
+
+use SuO0\StorageApi\Repository\Topology\LocationRepository;
+use SuO0\StorageApi\Repository\Topology\StockPlacementRepository;
+
+use SuO0\StorageApi\Repository\Inventory\StockRepository;
+use SuO0\StorageApi\Repository\Catalog\PartRepository;
+
+class AddStockService {
+    public function __Construct(
+        private LocationRepository $Location,
+        private StockPlacementRepository $StockPlacement,
+        private StockRepository $Stock,
+        private PartRepository $Part    
+    ) { }
+
+    public function execute(string $Address, int $Qcy, ?string $Article = null):void {
+        echo "Добавление стока по адресу $Address\n";
+
+        $Location = $this->Location->findByAddress($Address);
+        if($Location === null)
+            throw new \RuntimeException("Адресс $Address не существует");
+
+        $Part = ($Article === null) ? null : $this->Part->findOrCreate($Article);
+
+        // $Stock = $this->Stock->findByPartId($Part['Id']);
+        // if($Stock !== null)
+        //     throw new \RuntimeException("Асортимент с артикулом $Article уже существует");    
+        
+        $StockId = $this->Stock->add($Qcy, $Part['Id']);
+
+        $this->StockPlacement->add($Location['Id'], $StockId);
+
+        echo "Сток $StockId добавлен по адресу $Address\n";
+    }
+}
