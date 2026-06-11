02ccf509eeab9f20509524514e4bab4d5e13343a
su_o0 <plitikyura@gmail.com>
2026-06-11 19:52:20 +0300

    chore(Connection): - config

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -4,12 +4,12 @@ namespace WarehouseCore\Connection;
 class Connection {
     private static ?\PDO $instance = null;
     
-    public static function get(array $config): \PDO {
+    public static function get(mixed $config): \PDO {
         if (self::$instance === null) {
             self::$instance = new \PDO(      
-                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4",
-                $config['user'],
-                $config['password'],
+                "mysql:host={$config->host};dbname={$config->dbname};charset=utf8mb4",
+                $config->user,
+                $config->password,
                 [
                     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                     \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
