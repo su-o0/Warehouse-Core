07b4c03127d5487fef3f899eccf39270470f7d0e
su_o0 <plitikyura@gmail.com>
2026-06-13 00:25:52 +0300

    chore(Domain Exception)

--- src/Exception/Domain/StockException.php
+++ src/Exception/Domain/StockException.php
@@ -12,20 +12,6 @@ class StockException extends \RuntimeException {
         parent::__construct($message);
     }
 
-    public static function STOCK_PLACEMENT_ALREADY_EXISTS(): self {
-        return new self(
-            ErrorCode::STOCK_PLACEMENT_ALREADY_EXISTS,
-            'Stock placement already exists'
-        );
-    }
-
-    public static function STOCK_PLACEMENT_NOT_FOUND(): self {
-        return new self(
-            ErrorCode::STOCK_PLACEMENT_NOT_FOUND,
-            'Stock placement not found'
-        );
-    }
-
     public static function STOCK_ALREADY_EXISTS(): self {
         return new self(
             ErrorCode::STOCK_ALREADY_EXISTS,
