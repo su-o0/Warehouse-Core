c1b548cb31846ef5458a8ebbc0d74db35dfad009
su_o0 <plitikyura@gmail.com>
2026-08-31 17:05:57 +0300

    chore(api): update api config list

--- config/api.php
+++ config/api.php
@@ -10,7 +10,6 @@ return [
     'ActivateArea' => 'activate_area',
     'MarkAreaAsCrowded' => 'mark_area_as_crowded',
     'ArchiveArea' => 'archive_area',
-    'ListArea' => 'list_area',
 
     'AddZoneName' => 'add_zone_name',
     'SetPrimaryZoneName' => 'set_primary_zone_name',
@@ -20,8 +19,26 @@ return [
     'MarkZoneaAsCrowded' => 'mark_zone_as_crowded',
     'ArchiveZone' => 'archive_zone',
 
+    'ListArea' => 'list_area',
+    'ListAreaNames' => 'list_area_names',
+    'ListZoneByArea' => 'list_zone_by_area',
+    'ListZoneNames' => 'list_zone_names',
+    'ListUser' => 'list_user',
+    'ListUserNames' => 'list_user_names',
+    'ListUserIdentities' => 'list_user_identities',
 
     'CreateUser' => 'create_user',
+    'ActivateUser' => 'activate_user',
+    'ArchiveUser' => 'archive_user',
+    'AssignUserRole' => 'assign_user_role',
+    'DismissUserRole' => 'dismiss_user_role',
+    'AddUserIdentity' => 'add_user_identity',
+    'RemoveUserIdentity' => 'remove_user_identity',
+    'AddUserName' => 'add_user_name',
+    'SetPrimaryUserName' => 'set_primary_user_name',
+    'RemoveUserName' => 'remove_user_name',
+
+
     'CreateUserIdentity' => 'create_user_identity',
     'CreatePhysicalTag' => 'create_physical_tag',
     'CreateContainer' => 'create_container',
